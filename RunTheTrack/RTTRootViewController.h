22ad9c4976ef7a079ddadba504a3b811b7ed3792
Andrew Donnelly <andrew.donnelly4@bskyb.com>
2014-06-03 21:04:45 +0100

    Added menus

--- RunTheTrack/RTTRootViewController.h
+++ RunTheTrack/RTTRootViewController.h
@@ -1,14 +1,14 @@
 //
-//  RTTBaseViewController.h
+//  RTTRootViewController.h
 //  RunTheTrack
 //
-//  Created by Andrew Donnelly on 20/05/2014.
+//  Created by Andrew Donnelly on 03/06/2014.
 //  Copyright (c) 2014 iphonemobileapp. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "RESideMenu.h"
 
-@interface RTTBaseViewController : UIViewController 
+@interface RTTRootViewController : RESideMenu <RESideMenuDelegate>
 
 @end
